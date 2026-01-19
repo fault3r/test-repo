b86f199f5a65b9be5e9aa1008762f0e9fd15d463
Hamed Damavandi <hamed.damaavandi@gmail.com>
2026-01-19 21:27:54 +0330

    testing #2

--- src/CSTest/Program.cs
+++ src/CSTest/Program.cs
@@ -1,14 +1,44 @@
 ﻿
 using System;
+using System.Threading.Tasks;
 
 namespace CSTest;
 
 internal class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        Console.WriteLine("App started...");
+        Console.WriteLine("App started...\n");
 
-        Console.WriteLine("Everything is ended.");
+        var task = Task.Run(() => Loading());
+        Console.WriteLine("\nresumming...\n");
+
+        Thread.Sleep(5000);
+        stop = true;
+
+        await task;
+
+        Console.WriteLine("\nEverything is ended.");
+    }
+
+    public static bool stop = false;
+
+    public static Task Loading()
+    {
+        string text = "Damaavandi";
+
+        int left = Console.CursorLeft + 20;
+        int top = Console.CursorTop;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            Console.SetCursorPosition(left, top);
+            Console.Write(text[i..(i + 1)]);
+            Thread.Sleep(300);
+
+            if (i == text.Length-1) i = 0;
+            if (stop) break;
+        }
+        return Task.CompletedTask;
     }
 }
